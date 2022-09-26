b08fde80c447158a95a341695d8fcd5c95e0ea7d
yikezawa <y.ikezawa93@gmail.com>
2022-09-26 23:26:42 +0900

    mod

---
+++ Piece.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Piece {
+  private final Maze maze;
+  private Location location;
+  private Direction direction = Direction.NORTH;
+  private final List<Direction> history = new ArrayList<>();
+
+  
+}
