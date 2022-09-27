f5430ac76a7aa17ec8c7c24117012d781a8e5919
yikezawa <y.ikezawa93@gmail.com>
2022-09-27 23:08:03 +0900

    mod

--- Direction.java
+++ Direction.java
@@ -1,6 +1,6 @@
 public enum Direction {
   NORTH(0, -1), EAST(1, 0), SOUTH(0, 1), WEST(-1, 0);
-  private int dx, dy;
+  public int dx, dy;
   private Direction(int dx, int dy) {
     this.dx = dx;
     this.dy = dy;
--- Piece.java
+++ Piece.java
@@ -7,5 +7,19 @@ public class Piece {
   private Direction direction = Direction.NORTH;
   private final List<Direction> history = new ArrayList<>();
 
-  
+  public Piece(Maze maze) {
+    this.maze = maze;
+    this.location = maze.getLocation();
+  }
+
+  public void turnLeft() { direction = direction.left(); }
+  public void turnRight() { direction = direction.right(); }
+
+  public boolean tryStepForward() {
+    Location nextLocation = new Location(location.x + direction.dx, location.y + direction.dy);
+    if (maze.isBlank(nextLocation)) {
+      
+    }
+    return false;
+  }
 }
