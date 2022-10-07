73fa70cc18e899e510201a89eadd6a04b2102b9a
yikezawa <y.ikezawa93@gmail.com>
2022-10-07 22:46:50 +0900

    mod

---
+++ Template.java
@@ -0,0 +1,7 @@
+import java.util.List;
+import java.util.Map;
+
+
+public class Template {
+  List<Fragment> fragmentList;
+}
---
+++ TemplateParser.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class TemplateParser {
+  static public Template parse(Reader reader) throws IOException {
+    StringBuilder buf = new StringBuilder();
+
+
+  }
+}
