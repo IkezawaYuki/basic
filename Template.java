cf5346107fa1650092acebb2bba50a14f3c25807
yikezawa <y.ikezawa93@gmail.com>
2022-10-09 23:37:39 +0900

    mod

--- Template.java
+++ Template.java
@@ -8,4 +8,12 @@ public class Template {
   Template(List<Fragment> fragmentList) {
     this.fragmentList = fragmentList;
   }
+
+  public String apply(Map<String, List<String>> table) {
+    StringBuilder sb = new StringBuilder();
+    for (Fragment fragment : fragmentList) {
+      sb.append(fragment.replace(table));
+    }
+    return sb.toString();
+  }
 }
