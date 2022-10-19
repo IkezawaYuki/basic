d71d1c7aeddcb60313e559949ed772e45d9f52d1
yikezawa <y.ikezawa93@gmail.com>
2022-10-19 23:48:05 +0900

    mod

--- ValueSortedMap.java
+++ ValueSortedMap.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.TreeMap;
 
-
 public class ValueSortedMap<K, V> implements Iterable<K> {
   Map<K, V> map = new HashMap<K, V>();
   Map<V, List<K>> reverseMap;
@@ -17,65 +16,5 @@ public class ValueSortedMap<K, V> implements Iterable<K> {
     reverseMap = new TreeMap<V, List<K>>();
   }
 
-  public ValueSortedMap(Comparator<? super V> c) {
-    reverseMap = new TreeMap<V, List<K>>(c);
-  }
-
-  public V put(K key, V value) {
-    if (key == null || value == null) {
-      throw new NullPointerException();
-    }
-    V old = remove(key);
-    map.put(key, value);
-    List<K> keys = reverseMap.get(value);
-    if (keys == null) {
-      keys = new ArrayList<K>();
-      reverseMap.put(value, keys);
-    }
-    keys.add(key);
-    return old;
-  }
-
-  public V get(K key) {
-    return map.get(key);
-  }
-
-  public V remove(K key) {
-    V value = map.remove(key);
-    if (value != null) {
-      List<K> keys = reverseMap.get(value);
-      keys.remove(key);
-      if (keys.isEmpty()) {
-        reverseMap.remove(value);
-      }
-    }
-    return value;
-  }
-
-  public int size() {
-    return map.size();
-  }
-
-  public Iterator<K> iterator() {
-    return new Iterator<K>() {
-      Iterator<V> vi = reverseMap.keySet().iterator();
-      Iterator<K> ki = new ArrayList<K>().iterator();
-      public boolean hasNext() {
-        return vi.hasNext() || ki.hasNext();
-      }
-      public K next() {
-        if (hasNext()) {
-          if (!ki.hasNext()) {
-            ki = reverseMap.get(vi.next()).iterator();
-          }
-          return ki.next();
-        }
-        throw new NoSuchElementException();
-      }
-      
-      public void remove() {
-        throw new UnsupportedOperationException();
-      }
-    };
-  }
+  
 }
