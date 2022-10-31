a28ecea531bbb2d0dbd798fdb63bc1f22a38dcba
yikezawa <y.ikezawa93@gmail.com>
2022-10-31 23:33:01 +0900

    mod

---
+++ Randomfile.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Randomfile {
+  public static void main(String[] args) throws IOException{
+    Randomfile randomfile = new Randomfile();
+    randomfile.test();
+  }
+
+  void test() throws IOException {
+    
+  }
+}
