70b1c6b3bcdbc720ca838a1a2cd3d7704788cc62
yikezawa <y.ikezawa93@gmail.com>
2022-11-02 23:38:10 +0900

    mod

--- Randomfile.java
+++ Randomfile.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class Randomfile {
   public static void main(String[] args) throws IOException{
@@ -7,6 +8,17 @@ public class Randomfile {
   }
 
   void test() throws IOException {
+    String filename = "randf.dat";
+    int[] intArray = {10,20,30,40,50,60,70,80,90};
+    RandomAccessFile raf1 = new RandomAccessFile(filename, "rw");
+    for (int i = 0; i < intArray.length; i++) {
+      raf1.writeInt(intArray[i]);
+    }
+    raf1.close();
+    displayRandomFile(filename);
+  }
+
+  void displayRandomFile(String filename) throws IOException {
     
   }
 }
